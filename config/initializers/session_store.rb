d5c482dac04537f60c043092615e1b398148499e
Yutaka HARA <yutaka.hara+github@gmail.com>
2010-11-17 15:05:23 +0900

    trans_sid

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-ChizuMemo::Application.config.session_store :cookie_store, :key => '_chizu_memo_session'
+#ChizuMemo::Application.config.session_store :cookie_store, :key => '_chizu_memo_session'
+
+ChizuMemo::Application.config.session_store :active_record_store, :cookie_only => false
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
